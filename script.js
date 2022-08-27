f4eaa4a369de3692ddc5490901d17bf4a128bbf4
Salem Sayed <salemsayed981@gmail.com>
2022-08-27 02:36:14 +0200

    upload the design

---
+++ script.js
@@ -0,0 +1,61 @@
+
+/* 
+console.log(true && false && true)
+console.log( "" || 6 || true || 5 ||'ssd'  )
+
+ */
+/* var x = 10
+var myColor=(x<10)? 'red':"blue"
+
+document.write("Hello".fontcolor(myColor))
+console.log(myColor)
+ */
+
+
+
+
+
+// console.log(!!0n)
+
+// let a=[1,2,5,8,7]
+
+// /* for (let b of a) console.log(b)
+
+// for (let x in a) console.log(x)
+//  */
+
+// var arr = new Array()
+
+// arr.name = 'salim'
+// arr.age = 23
+
+
+// console.log(arr)
+
+// for (i of arr)
+//     console.log(arr[i])
+// console.log(forEach(arr))
+
+
+
+
+/* var x = prompt()
+
+console.log(x * 5)
+
+ */
+
+var x = prompt('enter')
+var num=parseInt(x,2)
+
+console.log(num)
+
+
+
+
+
+
+
+
+
+
